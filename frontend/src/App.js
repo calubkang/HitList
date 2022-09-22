062fd0709370e60da7a8b9f98006b3bdb6cd5c63
calubkang <clbkang@gmail.com>
2022-09-21 21:16:19 -0700

    Added resume link functionality, added accordion styling. Need to implement a modal to view single job application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -101,11 +101,11 @@ function App() {
   }
 
   const addResume = (hit) => {
-    const updatedHit = {...hit, resume: resume, reachedOut: !hit.reachedOut}
+    const updatedHit = { ...hit, resume: resume, reachedOut: !hit.reachedOut }
     listService.updateHit(hit, updatedHit)
-    .then(updatedHit => {
-      setListItems(listItems.map(hit => hit.id !== updatedHit.id ? hit : updatedHit))
-    })
+      .then(updatedHit => {
+        setListItems(listItems.map(hit => hit.id !== updatedHit.id ? hit : updatedHit))
+      })
   }
 
   // --------------------------------------------------
@@ -137,60 +137,141 @@ function App() {
 
       <h1 className='container mt-3 text-center display-2'>HitList</h1>
 
-      <table className='container'>
-        <tbody>
-          <tr className='header-row'>
-            <td>Company</td>
-            <td>Position</td>
-            <td>Contact</td>
-            <td>Email</td>
-            <td>Resume</td>
-            <td>Actions</td>
-          </tr>
-          {firstList.map(hit =>
-            <Row
-              key={hit.id}
-              hit={hit}
-              onDelete={() => deleteHit(hit)}
-              updateButtonLabel='Reached Out!'
-              onUpdate={() => toggleReach(hit)}
-              className='need-to-reach-out'
-              onSubmit={() => addResume(hit)}
-              resume={resume}
-              handleResumeChange={handleResumeChange}
-            />
-          )}
-          {reachedList.map(hit =>
-            <Row
-              key={hit.id}
-              hit={hit}
-              onDelete={() => deleteHit(hit)}
-              updateButtonLabel='Got an interview!'
-              onUpdate={() => toggleInterview(hit)}
-              className='reached-out'
-            />
-          )}
-          {interviewList.map(hit =>
-            <Row
-              key={hit.id}
-              hit={hit}
-              onDelete={() => deleteHit(hit)}
-              updateButtonLabel='Finished interviews!'
-              onUpdate={() => finishInterview(hit)}
-              className='landed-interview'
-            />
-          )}
-          {finishedList.map(hit =>
-            <Row
-              key={hit.id}
-              hit={hit}
-              onDelete={() => deleteHit(hit)}
-              className='finished-interview'
-            />
-          )}
-        </tbody>
-      </table>
-
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Need to Apply
+            </button>
+          </h2>
+          <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <table className='container'>
+                <tbody>
+                  <tr className='header-row'>
+                    <td>Company</td>
+                    <td>Position</td>
+                    <td>Contact</td>
+                    <td>Email</td>
+                    <td>Resume</td>
+                    <td>Actions</td>
+                  </tr>
+                  {firstList.map(hit =>
+                    <Row
+                      key={hit.id}
+                      hit={hit}
+                      onDelete={() => deleteHit(hit)}
+                      updateButtonLabel='Reached Out!'
+                      onUpdate={() => toggleReach(hit)}
+                      className='need-to-reach-out'
+                      onSubmit={() => addResume(hit)}
+                      resume={resume}
+                      handleResumeChange={handleResumeChange}
+                    />
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Waiting to hear back
+            </button>
+          </h2>
+          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <table className='container'>
+                <tbody>
+                  <tr className='header-row'>
+                    <td>Company</td>
+                    <td>Position</td>
+                    <td>Contact</td>
+                    <td>Email</td>
+                    <td>Resume</td>
+                    <td>Actions</td>
+                  </tr>
+                  {reachedList.map(hit =>
+                    <Row
+                      key={hit.id}
+                      hit={hit}
+                      onDelete={() => deleteHit(hit)}
+                      updateButtonLabel='Got an interview!'
+                      onUpdate={() => toggleInterview(hit)}
+                      className='reached-out'
+                    />
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Interview Prep
+            </button>
+          </h2>
+          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <table className='container'>
+                <tbody>
+                  <tr className='header-row'>
+                    <td>Company</td>
+                    <td>Position</td>
+                    <td>Contact</td>
+                    <td>Email</td>
+                    <td>Resume</td>
+                    <td>Actions</td>
+                  </tr>
+                  {interviewList.map(hit =>
+                    <Row
+                      key={hit.id}
+                      hit={hit}
+                      onDelete={() => deleteHit(hit)}
+                      updateButtonLabel='Finished interviews!'
+                      onUpdate={() => finishInterview(hit)}
+                      className='landed-interview'
+                    />
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingFour">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+              Waiting for Final Decision
+            </button>
+          </h2>
+          <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <table className='container'>
+                <tbody>
+                  <tr className='header-row'>
+                    <td>Company</td>
+                    <td>Position</td>
+                    <td>Contact</td>
+                    <td>Email</td>
+                    <td>Resume</td>
+                    <td>Actions</td>
+                  </tr>
+                  {finishedList.map(hit =>
+                    <Row
+                      key={hit.id}
+                      hit={hit}
+                      onDelete={() => deleteHit(hit)}
+                      className='finished-interview'
+                    />
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
       <NewItemForm
         onSubmit={addHit}
         newCompName={newCompName}
